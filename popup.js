f378c8b6411c5d011b458b995ba23cda57ebdf81
Víctor Oliva <olivarra1@gmail.com>
2021-10-15 14:34:15 +0200

    Add GUI

--- popup.js
+++ popup.js
@@ -7,18 +7,45 @@ async function init() {
     return;
   }
   setVisible(elements.main, true);
-  elements.main.innerHTML = "NumOfVideos: " + numberOfVideos;
+
+  elements.videoNum.querySelector("span").innerText = `[0-${numberOfVideos}]`;
 }
 
 const elements = {
   loading: document.querySelector("#loading"),
   noVideos: document.querySelector("#no-videos"),
   main: document.querySelector("#main"),
+  videoNum: document.querySelector("#video-num"),
+  low_t: document.querySelector("#low_t"),
+  low_s: document.querySelector("#low_s"),
+  high_t: document.querySelector("#high_t"),
+  high_s: document.querySelector("#high_s"),
+  play: document.querySelector("#play"),
+  stop: document.querySelector("#stop"),
 };
 const setVisible = (element, visible) =>
   visible
     ? element.classList.remove("hidden")
     : element.classList.add("hidden");
+
+elements.play.addEventListener("click", () => {
+  const paramElements = {
+    low_t: elements.low_t,
+    low_s: elements.low_s,
+    high_t: elements.high_t,
+    high_s: elements.high_s,
+  };
+  const params = Object.fromEntries(
+    Object.entries(paramElements).map(([key, element]) => [
+      key,
+      Number(element.querySelector("input").value),
+    ])
+  );
+  const n = Number(elements.videoNum.querySelector("input").value);
+  play(params, n);
+});
+
+elements.stop.addEventListener("click", stop);
 
 init();
 
@@ -37,4 +64,22 @@ async function getNumberOfVideos() {
   });
 
   return Number(result[0].result);
+}
+
+async function play(params, n) {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    args: [params, n],
+    function: (params, n) => window.SHAVE_SILENCE.play(params, n),
+  });
+}
+async function stop() {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: () => window.SHAVE_SILENCE.stop(),
+  });
 }
